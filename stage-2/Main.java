de0ac847e0568b92ac3758659cae14a743eac27d
henok-9 <23005635+henok-9@users.noreply.github.com>
2024-03-19 10:44:05 -0400

    gitingnore
    	new file:   .gitignore
    	modified:   stage-2/Main.java

--- stage-2/Main.java
+++ stage-2/Main.java
@@ -1,12 +1,24 @@
-
 public class Main {
-    public static void main(String[] args) {
-        GridState gridState = new GridState(6,3);
+   public static void main(String[] args) {
+        int row = 4;
+        int col = 4;
+        int seed = 4;
+        int numGenerations = 1;
+
+        String[][] grid = new String[row][col];
+        GridState gridState = new GridState(grid, grid.length, seed);
         gridState.generateGrid();
+        
+        
         gridState.printGrid();
-        System.out.println("==================================================");
-        // gridState.printEvolved();
-        gridState.printTest();
-        System.out.println(gridState.findNeighbors(gridState.getGrid(), 4, 2));
+        // System.out.println("=====================================");
+
+        grid = gridState.cellEvolution(grid, numGenerations); 
+        gridState.evolutionPrint(grid);  
+
     }
 }
+
+
+
+
