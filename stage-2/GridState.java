38092a92795b51906d5118b3ff0e6c5596bdd4bb
henok-9 <23005635+henok-9@users.noreply.github.com>
2024-03-19 10:47:23 -0400

    stage-2-submission

--- stage-2/GridState.java
+++ stage-2/GridState.java
@@ -1,26 +1,20 @@
-import java.util.Arrays;
 import java.util.Random;
-
 
 public class GridState{
     private int gridSize;
     private int seed;
+    private int numGenerations;
     Random random;
     private String[][] genZero;
-    // private String[][] evolved;
 
-    public GridState(String[][] grid, int gridSize, int seed) {
-        this.genZero = grid;
+    public GridState(int gridSize, int seed, int numGenerations) {
+        this.genZero = new String[gridSize][gridSize]; 
         this.gridSize = gridSize;
         this.seed = seed;
+        this.numGenerations = numGenerations;
         random = new Random(this.seed);
-        // genZero = new String[gridSize][gridSize];
-        // evolved = Arrays.copyOf(genZero, gridSize);
-        
-        // cellEvolution(genZero,3);
     }
 
-    // public String[][] generateGrid(String[][] grid, int gridSize) {
     public void generateGrid() {
         for (int i = 0; i < this.gridSize; i++) {
             for (int j = 0; j < this.gridSize; j++) {
@@ -30,54 +24,38 @@ public class GridState{
                     genZero[i][j] = " ";
             }
         }
-        // for (int i = 0; i < gridSize; i++) {
-        //     for (int j = 0; j < gridSize; j++) {
-        //         if (getGridState())
-        //             grid[i][j] = "O";
-        //         else
-        //             grid[i][j] = " ";
-        //     }
-        // }
-        // return grid;
     }
     
     public int findNeighbors(String[][] grid, int row, int col) {
-        // grid = genZero;
         int maxRows = grid.length;
         int maxCols = grid[0].length;
         String[] neighbors = new String[8];
         int numAliveNieghbors = 0;
-
         int[] neighborOffsetsRow  = {-1, -1, -1, 0, 0, 1, 1, 1};
         int[] neighborOffsetsCol = {-1, 0, 1, -1, 1, -1, 0, 1};
 
         for (int i = 0; i < 8; i++) {
-            // if (isCellAlive(grid, row, col)) {
                 int neighborRow = (row + neighborOffsetsRow[i] + maxRows) % maxRows;
                 int neighborCol = (col + neighborOffsetsCol[i] + maxCols) % maxCols;
                 neighbors[i] = grid[neighborRow][neighborCol];
-            // }
         }
         for (int i = 0; i < neighbors.length; i++) {
             if (isCellAlive(neighbors[i])) {
                 numAliveNieghbors++;
             }
         }
-        
         return numAliveNieghbors;
     }
     
-    public String[][] cellEvolution(int numGen) {
+    public String[][] cellEvolution() {
         String[][] nextGeneration = deepCopy(genZero);
-        for (int k = 0; k < numGen; k++) {
+        for (int k = 0; k < numGenerations; k++) {
             for (int i = 0; i < this.gridSize; i++) {
                 for (int j = 0; j < this.gridSize; j++) {
                     int neighbors = findNeighbors(genZero, i, j);
                     if (isCellAlive(genZero[i][j]) && (neighbors < 2 || neighbors > 3)) {
-                        // nextGeneration[i][j] = " ";
                         killCell(nextGeneration, i, j);
                     } else if (!isCellAlive(genZero[i][j]) && neighbors == 3) {
-                        // nextGeneration[i][j] = "O";
                         reviveCell(nextGeneration, i, j);
                     }
                 }
@@ -87,38 +65,17 @@ public class GridState{
         return genZero;
     }
 
-    // public String[][] cellEvolution(String[][] grid, int numGen) {
     public String[][] cellEvolution(String[][] evolved, int numGen) {
-        // String[][] evolved = new String[gridSize][gridSize];
         evolved = deepCopy(evolved); 
         for (int k = 0; k < numGen; k++) {
             String[][] nextGeneration = deepCopy(evolved); 
             for (int i = 0; i < this.gridSize; i++) {
                 for (int j = 0; j < this.gridSize; j++) {
-                    // System.out.print(findNeighbors(grid, i, j)+" ");
-                    // if (isCellAlive(grid, i, j) && findNeighbors(grid, i, j) < 2 || 
-                     
                     if (isCellAlive(evolved, i, j) && findNeighbors(evolved, i, j) < 2 || findNeighbors(evolved, i, j) > 3)  {
-                        // System.out.println("Cell Killed at: " + i + ", " + j); 
                         killCell(nextGeneration, i, j);
-                        // System.out.print("[K: "+killCell(grid, i, j)+"Neig: "+findNeighbors(grid, i, j)+" Idx: "+i+", "+j+"] ");
-                        // System.out.println(Arrays.toString(grid[i]) + Arrays.toString(grid[j])); 
-                        // evolved[i][j] = grid[i][j];
                     }
-                    // if (isCellAlive(nextGeneration, i, j) && findNeighbors(nextGeneration, i, j) > 3){
-                    //     // System.out.println("Cell Killed at: " + i + ", " + j); 
-                    // // if (findNeighbors(grid, i, j) > 3){
-                    //     killCell(nextGeneration, i, j);
-                    //     // killCell(grid, i, j);
-                    //     // evolved[i][j] = grid[i][j];
-                    //     // System.out.print("[K: "+killCell(grid, i, j)+"Neig: "+findNeighbors(grid, i, j)+" Idx: "+i+", "+j+"] ");
-                    // }
                     else if(!isCellAlive(evolved, i, j) && findNeighbors(evolved, i, j) == 3){
-                        // System.out.println("Cell Revived at: " + i + ", " + j); 
                         reviveCell(nextGeneration, i, j);
-                        // System.out.print("[R: "+reviveCell(grid, i, j)+" Neig: "+findNeighbors(grid, i, j)+" Idx: "+i+", "+j+"] ");
-                        // System.out.print("check: "+findNeighbors(grid, i, j)+" ");
-                        // evolved[i][j] = grid[i][j];
                     }
                 }
             }
@@ -126,7 +83,6 @@ public class GridState{
         }   
         return evolved;
     }
-    
     
     public String[][] deepCopy(String[][] grid) {
         String[][] copy = new String[grid.length][grid[0].length];
@@ -162,32 +118,12 @@ public class GridState{
     
     public void printGrid() {
         for (int i = 0; i < gridSize; i++) {
-            System.out.println(Arrays.toString(genZero[i]));
+            for (int j = 0; j < gridSize; j++) {
+                System.out.print(genZero[i][j]);
+            }
+            System.out.println();
         }
     }
-    // public void printEvolved() {
-    //     for (int i = 0; i < gridSize; i++) {
-    //         System.out.println(Arrays.toString(evolved[i]));
-    //     }
-    // }
-    
-    public void evolutionPrint(String[][] grid) {
-        // grid = cellEvolution(genZero);
-        for (int i = 0; i < gridSize; i++) {
-            System.out.println(Arrays.toString(grid[i]));
-        }
-    }
-   
-    //duplicate 
-    // public String[][] _cellEvolution(String[][] grid, String[][] evolved, int numGen) {
-    //     for (int i = 0; i < grid.length; i++) {
-    //         for (int j = 0; j < grid.length; j++) {
-    //         }
-    //     }
-    //     return evolved;
-    // }
-    //
-    // 
 } 
 
 
--- stage-2/Main.java
+++ stage-2/Main.java
@@ -1,20 +1,18 @@
-public class Main {
-   public static void main(String[] args) {
-        int row = 4;
-        int col = 4;
-        int seed = 4;
-        int numGenerations = 1;
+import java.util.Scanner;
 
-        String[][] grid = new String[row][col];
-        GridState gridState = new GridState(grid, grid.length, seed);
-        gridState.generateGrid();
+class Main {
+   public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in); 
+        int gridSize = scnr.nextInt();
+        int seed = scnr.nextInt();
+        int numGenerations = scnr.nextInt();
         
+        GridState gridState = new GridState(gridSize, seed, numGenerations);
+        
+        gridState.generateGrid();
+        gridState.cellEvolution(); 
         
         gridState.printGrid();
-        System.out.println("=====================================");
-
-        grid = gridState.cellEvolution(grid, numGenerations); 
-        gridState.evolutionPrint(grid);  
 
     }
 }
